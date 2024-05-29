7dbcf1566a64c5a6141ee4f1ce6e871383a9a742
silvercrowkws <silvercrowkws@naver.com>
2024-05-29 17:58:08 +0900

    Vcam 수정

--- Assets/Scripts/Player/Players.cs
+++ Assets/Scripts/Player/Players.cs
@@ -97,7 +97,8 @@ public class Players : MonoBehaviour
         leftPlayerAnimator = leftPlayer.GetComponent<Animator>();
         leftRigidbody = leftPlayer.GetComponent<Rigidbody2D>();
         //leftCollider = leftPlayer.GetComponent<Collider2D>();
-        leftPosition = leftPlayer.transform;
+        //leftPosition = leftPlayer.transform;
+        
 
         child = transform.GetChild(1);                                  // 1번째 자식 rightPlayer
         rightPlayer = child.gameObject;
@@ -114,21 +115,30 @@ public class Players : MonoBehaviour
         buttonController.onRightJump += RightJump;
 
         // 'LeftVcam' 태그를 가진 CinemachineVirtualCamera를 찾음
-        CinemachineVirtualCamera leftVcam = GameObject.FindGameObjectWithTag("LeftVcam").GetComponent<CinemachineVirtualCamera>();
-
+        leftVcam = GameObject.FindGameObjectWithTag("LeftVcam").GetComponent<CinemachineVirtualCamera>();
+        
         // 찾은 카메라의 Follow를 leftPosition으로 설정
-        leftVcam.Follow = leftPosition;
+        leftVcam.LookAt = leftPlayer.transform;
 
         // 'RightVcam' 태그를 가진 CinemachineVirtualCamera를 찾음
-        CinemachineVirtualCamera rightVcam = GameObject.FindGameObjectWithTag("RightVcam").GetComponent<CinemachineVirtualCamera>();
+        rightVcam = GameObject.FindGameObjectWithTag("RightVcam").GetComponent<CinemachineVirtualCamera>();
 
         // 찾은 카메라의 Follow를 rightPosition으로 설정
-        rightVcam.Follow = rightPosition;        
+        rightVcam.LookAt = rightPlayer.transform;
     }
 
     private void Update()
     {
         //rightVcam.transform.position = new Vector3(rightPosition.position.x, rightPosition.position.y, rightPosition.position.z);
+        // 'LeftVcam' 태그를 가진 CinemachineVirtualCamera를 찾음
+        //CinemachineVirtualCamera leftVcam = GameObject.FindGameObjectWithTag("LeftVcam").GetComponent<CinemachineVirtualCamera>();
+        leftPosition = leftPlayer.transform;
+        leftVcam.transform.position = new Vector3(-5, leftPosition.position.y, -10);
+
+        // 'RightVcam' 태그를 가진 CinemachineVirtualCamera를 찾음
+        //CinemachineVirtualCamera rightVcam = GameObject.FindGameObjectWithTag("RightVcam").GetComponent<CinemachineVirtualCamera>();
+        rightPosition = leftPlayer.transform;
+        rightVcam.transform.position = new Vector3(5, rightPosition.position.y, -10);
     }
 
     /// <summary>
@@ -163,7 +173,7 @@ public class Players : MonoBehaviour
             Vector2 forceDirection = Vector2.left * leftDirection;
 
             // 이전에 가해지던 힘 제거하고
-            rightRigidbody.velocity = Vector2.zero;
+            leftRigidbody.velocity = Vector2.zero;
 
             // 플레이어의 Rigidbody에 힘을 가한다
             leftRigidbody.AddForce(forceDirection * jumpSpeed, ForceMode2D.Impulse);
@@ -190,7 +200,7 @@ public class Players : MonoBehaviour
             // 클릭 횟수에 따라 회전 각도 결정
             if (rightClickCount % 2 == 1)
             {
-                targetRotationY = 180f;            
+                targetRotationY = 180f;
             }
 
             // 방향 결정
@@ -200,7 +210,7 @@ public class Players : MonoBehaviour
             rightPlayer.transform.rotation = Quaternion.Euler(0f, targetRotationY, 0f);
 
             // 회전된 방향으로 힘을 가하기 위해 회전된 방향 벡터를 계산
-            //Vector2 forceDirection = rightPlayer.transform.forward;
+            //Vector2 forceDirection = leftPlayer.transform.forward;
 
             // 왼쪽 플레이어의 경우
             Vector2 forceDirection = Vector2.right * rightDirection;
